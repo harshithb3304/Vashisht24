55f3edcfa0d587e8ae3b3a04aece140ad5e71d38
harshithb3304 <harsh03032004@gmail.com>
2024-01-26 15:10:29 +0530

    Initial Commit

---
+++ src/app/(components)/navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from '@/app/styles/navbar.module.css';
+const Navbar = () => {
+    return (
+        <div className = {styles.nav} >
+            This is the navbar
+        </div>
+    );
+};
+
+export default Navbar;
