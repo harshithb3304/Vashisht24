4ddd1fecf615123806019369a12547c1c0fa14cf
harshithb3304 <harsh03032004@gmail.com>
2024-01-26 20:39:50 +0530

    Simcard with button

--- src/app/page.js
+++ src/app/page.js
@@ -2,9 +2,14 @@ import React from 'react';
 import styles from '@/app/styles/page.module.css'
 import Simcard from "@/app/(components)/simcard";
 const Page = () => {
+    const simcardData = [
+        { text: 'Card 1', imageSrc: '/images/simcard.svg' },
+        { text: 'Card 2', imageSrc: '/images/simcard.svg' },
+
+    ]
     return (
         <div className={styles.main}>
-          <Simcard/>
+            <Simcard data={simcardData} />
         </div>
     );
 };
