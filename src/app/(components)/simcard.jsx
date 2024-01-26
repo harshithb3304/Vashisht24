6d2737a9a1fec8e7b72a0d84baefee8fbcf9edc9
harshithb3304 <harsh03032004@gmail.com>
2024-01-26 16:02:45 +0530

    Simcard

---
+++ src/app/(components)/simcard.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import styles from '../styles/simcard.module.css';
+
+const Simcard = () => {
+        return (
+            <main>
+                    <div>hi</div>
+                    <div className={styles.yourClass}>
+                            <div className={styles.rectangle42}></div>
+                            <div className={styles.rectangle44}></div>
+                            <div className={styles.rectangle45}></div>
+                            <div className={styles.rectangle46}></div>
+                            <div className={styles.rectangle47}></div>
+                            <div className={styles.rectangle48}></div>
+                            <div className={styles.rectangle49}></div>
+                            <div className={styles.rectangle50}></div>
+                            <div className={styles.rectangle51}></div>
+                            <div className={styles.rectangle52}></div>
+                            <div className={styles.rectangle53}></div>
+                            <div className={styles.rectangle54}></div>
+                            <div className={styles.rectangle55}></div>
+                            <div className={styles.rectangle56}></div>
+                            <div className={styles.rectangle57}></div>
+                            <div className={styles.rectangle58}></div>
+                            <div className={styles.rectangle59}></div>
+                            <div className={styles.rectangle60}></div>
+                            <div className={styles.rectangle61}></div>
+                            <div className={styles.rectangle62}></div>
+                            <div className={styles.rectangle63}></div>
+                            <div className={styles.rectangle64}></div>
+                            <div className={styles.rectangle65}></div>
+                            <div className={styles.rectangle66}></div>
+                            <div className={styles.rectangle67}></div>
+                            <div className={styles.rectangle68}></div>
+                            <div className={styles.rectangle69}></div>
+                            <div className={styles.rectangle70}></div>
+                            <div className={styles.rectangle71}></div>
+                    </div>
+                    <div>
+                            <img src="/simcard.svg" alt="SVG Image"/>
+                    </div>
+            </main>
+        );
+};
+
+export default Simcard;
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import styles from '@/app/styles/page.module.css'
+import Simcard from "@/app/(components)/simcard";
 const Page = () => {
     return (
         <div className={styles.main}>
-          HI
+          <Simcard/>
         </div>
     );
 };
