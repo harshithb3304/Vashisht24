e40966aa5be5d623bce6747953364fcddc07523b
harshithb3304 <harsh03032004@gmail.com>
2024-02-17 15:02:32 +0530

    Mobile view of Dynamic Routes

--- src/app/workshops/page.jsx
+++ src/app/workshops/page.jsx
@@ -1,150 +1,155 @@
 // pages/Page.js
-import React from 'react';
-import styles from '@/app/styles/workshops.module.css';
-import Simcard from '/src/app/(components)/simcard';
+import React from "react";
+import styles from "@/app/styles/workshops.module.css";
+import Simcard from "/src/app/(components)/simcard";
 import Navbar from "@/app/(components)/navbar";
 import Footer from "@/app/(components)/footer";
 
 const simcardData = [
-    {
-        eventId: 'workshop1',
-        eventName: 'WORKSHOP 1',
-        imageSrc: '/images/ball.png',
-        eventInfo: 'Join us for an introductory workshop on React. Learn the basics and get started with building web applications.',
-        eventPrize: "₹100000",
-        eventDate : "16th February 2024",
-        eventTime : "10:00 PM",
-
-    },
-    {
-        eventId: 'workshop2',
-        eventName: 'WORKSHOP 2',
-        imageSrc: '/images/ball.png',
-        eventInfo: 'Explore advanced CSS techniques and best practices. Dive into responsive design, animations, and more.',
-        eventPrize: "₹20000000",
-        eventDate : "16th February 2024",
-        eventTime : "10:00 PM",
-    },
-    {
-        eventId: 'workshop3',
-        eventName: 'WORKSHOP 3',
-        imageSrc: '/images/ball.png',
-        eventInfo: 'Explore advanced CSS techniques and best practices. Dive into responsive design, animations, and more.',
-        eventPrize: "₹20000000",
-        eventDate : "16th February 2024",
-        eventTime : "10:00 PM",
-    },
-    {
-        eventId: 'workshop4',
-        eventName: 'WORKSHOP 4',
-        imageSrc: '/images/ball.png',
-        eventInfo: 'Explore advanced CSS techniques and best practices. Dive into responsive design, animations, and more.',
-        eventPrize: "₹20000000",
-        eventDate : "16th February 2024",
-        eventTime : "10:00 PM",
-    },
-    {
-        eventId: 'workshop5',
-        eventName: 'WORKSHOP 5',
-        imageSrc: '/images/ball.png',
-        eventInfo: 'Explore advanced CSS techniques and best practices. Dive into responsive design, animations, and more.',
-        eventPrize: "₹20000000",
-        eventDate : "16th February 2024",
-        eventTime : "10:00 PM",
-    },
-    {
-        eventId: 'workshop6',
-        eventName: 'WORKSHOP 6',
-        imageSrc: '/images/ball.png',
-        eventInfo: 'Explore advanced CSS techniques and best practices. Dive into responsive design, animations, and more.',
-        eventPrize: "₹20000000",
-        eventDate : "16th February 2024",
-        eventTime : "10:00 PM",
-    },
-    {
-        eventId: 'workshop7',
-        eventName: 'WORKSHOP 7',
-        imageSrc: '/images/ball.png',
-        eventInfo: 'Explore advanced CSS techniques and best practices. Dive into responsive design, animations, and more.',
-        eventPrize: "₹20000000",
-        eventDate : "16th February 2024",
-        eventTime : "10:00 PM",
-    },
-    {
-        eventId: 'workshop8',
-        eventName: 'WORKSHOP 8',
-        imageSrc: '/images/ball.png',
-        eventInfo: 'Explore advanced CSS techniques and best practices. Dive into responsive design, animations, and more.',
-        eventPrize: "₹20000000",
-        eventDate : "16th February 2024",
-        eventTime : "10:00 PM",
-    },
-    {
-        eventId: 'workshop9',
-        eventName: 'WORKSHOP 9',
-        imageSrc: '/images/ball.png',
-        eventInfo: 'Explore advanced CSS techniques and best practices. Dive into responsive design, animations, and more.',
-        eventPrize: "₹20000000",
-        eventDate : "16th February 2024",
-        eventTime : "10:00 PM",
-    },
-    // Add more simcard data as needed
+  {
+    eventId: "workshop1",
+    eventName: "WORKSHOP 1",
+    imageSrc: "/images/ball.png",
+    eventInfo:
+      "Join us for an introductory workshop on React. Learn the basics and get started with building web applications.",
+    eventPrize: "₹100000",
+    eventDate: "16th February 2024",
+    eventTime: "10:00 PM",
+  },
+  {
+    eventId: "workshop2",
+    eventName: "WORKSHOP 2",
+    imageSrc: "/images/ball.png",
+    eventInfo:
+      "Explore advanced CSS techniques and best practices. Dive into responsive design, animations, and more.",
+    eventPrize: "₹20000000",
+    eventDate: "16th February 2024",
+    eventTime: "10:00 PM",
+  },
+  {
+    eventId: "workshop3",
+    eventName: "WORKSHOP 3",
+    imageSrc: "/images/ball.png",
+    eventInfo:
+      "Explore advanced CSS techniques and best practices. Dive into responsive design, animations, and more.",
+    eventPrize: "₹20000000",
+    eventDate: "16th February 2024",
+    eventTime: "10:00 PM",
+  },
+  {
+    eventId: "workshop4",
+    eventName: "WORKSHOP 4",
+    imageSrc: "/images/ball.png",
+    eventInfo:
+      "Explore advanced CSS techniques and best practices. Dive into responsive design, animations, and more.",
+    eventPrize: "₹20000000",
+    eventDate: "16th February 2024",
+    eventTime: "10:00 PM",
+  },
+  {
+    eventId: "workshop5",
+    eventName: "WORKSHOP 5",
+    imageSrc: "/images/ball.png",
+    eventInfo:
+      "Explore advanced CSS techniques and best practices. Dive into responsive design, animations, and more.",
+    eventPrize: "₹20000000",
+    eventDate: "16th February 2024",
+    eventTime: "10:00 PM",
+  },
+  {
+    eventId: "workshop6",
+    eventName: "WORKSHOP 6",
+    imageSrc: "/images/ball.png",
+    eventInfo:
+      "Explore advanced CSS techniques and best practices. Dive into responsive design, animations, and more.",
+    eventPrize: "₹20000000",
+    eventDate: "16th February 2024",
+    eventTime: "10:00 PM",
+  },
+  {
+    eventId: "workshop7",
+    eventName: "WORKSHOP 7",
+    imageSrc: "/images/ball.png",
+    eventInfo:
+      "Explore advanced CSS techniques and best practices. Dive into responsive design, animations, and more.",
+    eventPrize: "₹20000000",
+    eventDate: "16th February 2024",
+    eventTime: "10:00 PM",
+  },
+  {
+    eventId: "workshop8",
+    eventName: "WORKSHOP 8",
+    imageSrc: "/images/ball.png",
+    eventInfo:
+      "Explore advanced CSS techniques and best practices. Dive into responsive design, animations, and more.",
+    eventPrize: "₹20000000",
+    eventDate: "16th February 2024",
+    eventTime: "10:00 PM",
+  },
+  {
+    eventId: "workshop9",
+    eventName: "WORKSHOP 9",
+    imageSrc: "/images/ball.png",
+    eventInfo:
+      "Explore advanced CSS techniques and best practices. Dive into responsive design, animations, and more.",
+    eventPrize: "₹20000000",
+    eventDate: "16th February 2024",
+    eventTime: "10:00 PM",
+  },
+  // Add more simcard data as needed
 ];
 
 const headingStyles = {
-    fontFamily: 'VCR OSD Mono, monospace',
-    fontSize: '30px',
-    fontWeight: 400,
-    lineHeight: '20px',
-    letterSpacing: '0em',
-    textAlign: 'left',
-    textDecorationColor: '#FFFFFF',
-    color: '#FFFFFF',
-    display: 'block',
+  fontFamily: "VCR OSD Mono, monospace",
+  fontSize: "30px",
+  fontWeight: 400,
+  lineHeight: "20px",
+  letterSpacing: "0em",
+  textAlign: "left",
+  textDecorationColor: "#FFFFFF",
+  color: "#FFFFFF",
+  display: "block",
 };
 
 const contentStyles = {
-    fontFamily: 'Anonymous Pro, monospace',
-    fontSize: '20px',
-    fontWeight: '400',
-    lineHeight: '20px',
-    letterSpacing: '0e',
-    textAlign: 'center',
-    width: '807px',
-    height: '100px',
-    top: '248px',
-    left: '236px',
-    textDecorationColor: '#FFFFFF',
-    color: '#FFFFFF',
-    display: 'block',
-    marginTop: '30px',
+  fontFamily: "Anonymous Pro, monospace",
+  fontSize: "20px",
+  fontWeight: "400",
+  lineHeight: "20px",
+  letterSpacing: "0e",
+  textAlign: "center",
+  width: "807px",
+  height: "100px",
+  top: "248px",
+  left: "236px",
+  textDecorationColor: "#FFFFFF",
+  color: "#FFFFFF",
+  display: "block",
+  marginTop: "30px",
 };
 
 const Page = () => {
-    return (
-        <main className={styles.workshopMain}>
-            <Navbar/>
-            <div className={styles.workshopInfo}>
-                <div style={headingStyles}>WORKSHOPS</div>
-                <div style={contentStyles}>
-                    Book a Workshop at Vashisht 24 and unlock FREE access to EDM Night, Robowars, Guest Lectures,
-                    Exhibitions, Summits, and Gaming setups. Limited seats – seize the chance to blend learning with
-                    entertainment!
-                </div>
-            </div>
-            <div className={styles.cardLayout}>
-                {simcardData.map((item, index) => (
-                    <Simcard key={item.eventId} item={item}/>
-                ))}
-            </div>
+  return (
+    <main className={styles.workshopMain}>
+      <Navbar />
+      <div className={styles.workshopInfo}>
+        <div style={headingStyles}>WORKSHOPS</div>
+        <div style={contentStyles}>
+          Book a Workshop at Vashisht 24 and unlock FREE access to EDM Night,
+          Robowars, Guest Lectures, Exhibitions, Summits, and Gaming setups.
+          Limited seats – seize the chance to blend learning with entertainment!
+        </div>
+      </div>
+      <div className={styles.cardLayout}>
+        {simcardData.map((item, index) => (
+          <Simcard key={item.eventId} item={item} />
+        ))}
+      </div>
 
-            <Footer/>
-
-
-
-        </main>
-    );
+      <Footer />
+    </main>
+  );
 };
 
 export default Page;
-export {simcardData };
+export { simcardData };
